8699d631c9d5926fadec912c054f69ecffbfbd41
Angora888 <angora88@gmail.com>
2026-09-12 10:07:33 -0600

    Add accounts receivable domain models

---
+++ BarberiaSaaS.Api/Models/EstadosCuentaPorCobrar.cs
@@ -0,0 +1,10 @@
+namespace BarberiaSaaS.Api.Models
+{
+    public static class EstadosCuentaPorCobrar
+    {
+        public const string Pendiente = "Pendiente";
+        public const string Parcial = "Parcial";
+        public const string Pagada = "Pagada";
+        public const string Anulada = "Anulada";
+    }
+}
