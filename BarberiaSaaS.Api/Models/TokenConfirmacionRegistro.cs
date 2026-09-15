3a7edd4571e6ecc29956118a72c5309700e44531
Angora888 <angora88@gmail.com>
2026-09-15 09:26:51 -0600

    Add registration confirmation token model

---
+++ BarberiaSaaS.Api/Models/TokenConfirmacionRegistro.cs
@@ -0,0 +1,25 @@
+using System.ComponentModel.DataAnnotations;
+using Microsoft.EntityFrameworkCore;
+
+namespace BarberiaSaaS.Api.Models
+{
+    [Index(nameof(TokenHash), IsUnique = true)]
+    public class TokenConfirmacionRegistro
+    {
+        public int Id { get; set; }
+
+        public int TenantId { get; set; }
+
+        [Required]
+        [MaxLength(64)]
+        public string TokenHash { get; set; } = string.Empty;
+
+        public DateTime FechaCreacion { get; set; } = DateTime.UtcNow;
+
+        public DateTime FechaExpiracion { get; set; }
+
+        public DateTime? FechaUso { get; set; }
+
+        public Tenant Tenant { get; set; } = null!;
+    }
+}
