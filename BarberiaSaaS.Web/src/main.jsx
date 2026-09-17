739858ea1107fca00fbb48a07fdf0e0e41741ecd
Angora888 <angora88@gmail.com>
2026-09-16 21:38:32 -0600

    Load mobile daily agenda refinements

--- BarberiaSaaS.Web/src/main.jsx
+++ BarberiaSaaS.Web/src/main.jsx
@@ -5,6 +5,7 @@ import "bootstrap/dist/css/bootstrap.min.css";
 import "./index.css";
 import "./responsive-overrides.css";
 import "./hide-cancelled-appointments.css";
+import "./agenda-mobile-overrides.css";
 import App from "./App.jsx";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
