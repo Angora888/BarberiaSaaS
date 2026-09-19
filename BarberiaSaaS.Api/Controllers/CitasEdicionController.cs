200412f478931d3f2ace59aee7c22b7e850b97b6
Angora888 <angora88@gmail.com>
2026-09-18 20:12:28 -0600

    Prevent appointments during professional lunch breaks

--- BarberiaSaaS.Api/Controllers/CitasEdicionController.cs
+++ BarberiaSaaS.Api/Controllers/CitasEdicionController.cs
@@ -181,6 +181,20 @@ namespace BarberiaSaaS.Api.Controllers
                 });
             }
 
+            var chocaConAlmuerzo = await _context.AlmuerzosProfesionales
+                .AnyAsync(x =>
+                    x.TenantId == tenantId &&
+                    x.ProfesionalId == profesional.Id &&
+                    x.DiaSemana == inicioLocal.DayOfWeek &&
+                    x.Activo &&
+                    inicioLocal.TimeOfDay < x.HoraFin &&
+                    finLocal.TimeOfDay > x.HoraInicio);
+
+            if (chocaConAlmuerzo)
+            {
+                return Conflict(new { mensaje = "La nueva hora coincide con la hora de almuerzo del profesional." });
+            }
+
             var chocaConCita = await _context.Citas
                 .AnyAsync(x =>
                     x.Id != cita.Id &&
